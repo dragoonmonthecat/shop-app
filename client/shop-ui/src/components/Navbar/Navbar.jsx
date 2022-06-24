d059fdec89d8682ba1004a2ff16a226753439762
dragoonmonthecat <gurpritjandu23454@gmail.com>
2022-06-24 23:44:29 +0100

    added icons

--- client/shop-ui/src/components/Navbar/Navbar.jsx
+++ client/shop-ui/src/components/Navbar/Navbar.jsx
@@ -2,6 +2,8 @@ import { React } from 'react';
 import './Navbar.css'
 import Logo from '../../Assets/Images/HestiaLogo.png';
 import SearchBar from '../Utilities/SearchBar/SearchBar';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import PersonIcon from '@mui/icons-material/Person';
 
 const Navbar = () =>
 {
@@ -9,7 +11,10 @@ const Navbar = () =>
         <div className='navbar-container'>
             <img src={Logo} alt='brand-logo' className='brand-logo'></img>
             <SearchBar />
-            <h5>Profile/Basket</h5>
+            <div className='basket-account-container'>
+                <ShoppingBasketIcon />
+                <PersonIcon />
+            </div>
         </div>
     );
 }
